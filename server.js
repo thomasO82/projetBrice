849f7afead520553bf56a02aeb57054b9185e485
thomasO82 <thomas.oliver@ri7.fr>
2022-06-29 13:17:04 +0000

    yaya

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ import express from 'express';
 import mongoose from 'mongoose';
 import userRouter from './routes/user.js';
 import 'dotenv/config';
-
+import cors from 'cors'
 
 const db = process.env.BDD_URL;
 const app = express();
 const router = express.Router()
 
+app.use(cors())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('./assets')); 
