91a35d3a6727568a2f12d295196a82a5cbda540b
thomasO82 <thomas.oliver@ri7.fr>
2023-01-07 20:41:09 +0100

    security

--- models/user.js
+++ models/user.js
@@ -5,20 +5,44 @@ const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: [true, 'Pas de nom'],
+        validate: {
+            validator: function(v) {
+                return /^[a-zA-Z]*$/.test(v);
+            },
+            message: "Please enter a valid name"
+        },
     },
     firstname: {
         type: String,
         required: [true, 'Pas de prénom'],
+        validate: {
+            validator: function(v) {
+                return /^[a-zA-Z]*$/.test(v);
+            },
+            message: "Please enter a valid firstname"
+        },
     },
     mail: {
         type: String,
         required: [true, 'Pas de mail'],
+        validate: {
+            validator: function(v) {
+                return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v);
+            },
+            message: "Please enter a valid email"
+        },
     },
     password: {
         type: String,
-        required: [true, 'Pas de mot de passe']
+        required: [true, 'Pas de mot de passe'],
+        validate: {
+            validator: function(v) {
+                return /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/.test(v);
+            },
+            message: "Please enter a valid password"
+        },
     },
-   
+
 })
 
 const UserModel = mongoose.model('User', userSchema)
